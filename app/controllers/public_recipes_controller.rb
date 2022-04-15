86d8d98237f0dc08f09e6c8ab6b783a1e4617aaf
unknown <azeemolatunji@gmail.com>
2022-04-15 20:12:18 +0100

    fix linters

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -2,6 +2,5 @@ class PublicRecipesController < ApplicationController
   def index
     @public_recipes = Recipe.where(public: true).order(created_at: :desc)
     @public_recipe = Recipe.new
-
   end
 end
