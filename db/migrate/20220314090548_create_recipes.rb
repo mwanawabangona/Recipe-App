5564bd591c63f44d0e9b5b16c8cb9b17cafd14b4
unknown <azeemolatunji@gmail.com>
2022-04-15 14:21:00 +0100

    clean db

--- db/migrate/20220314090548_create_recipes.rb
+++ db/migrate/20220314090548_create_recipes.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.integer :preperation_time
+      t.integer :preparation_time
       t.integer :cooking_time
-      t.text :description
+      t.string :description
       t.boolean :public
 
       t.timestamps
